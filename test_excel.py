f1c3eb7244608e1da447d8bf9449e0b5093bdd7d
TPogacar <pogacar.tami@gmail.com>
2020-12-27 12:07:32 +0100

    testirala branje

--- test_excel.py
+++ test_excel.py
@@ -13,6 +13,13 @@ print(os.path.exists(IME_DATOTEKE_S_PODATKI_2))
 dat_1 = xlrd.open_workbook(IME_DATOTEKE_S_PODATKI_1)
 dat_2 = xlrd.open_workbook(IME_DATOTEKE_S_PODATKI_2)
 
+list1 = dat_1.sheet_by_index(1)
+list2 = dat_2.sheet_by_index(1)
+print(list1.cell_value(1, 0))
+print(list1.cell_value(1, 1))
+print(list2.cell_value(1, 0))
+print(list2.cell_value(1, 1))
+
 # print(os.path.exists(r'D:\PB1\baza-Ekol\001_Evidenca_odpadko_v_skladiscu.xlsm'))
 # print(os.path.exists(os.path.join(sys.path[0], "001_Evidenca_odpadko_v_skladiscu.xlsm")))
 
