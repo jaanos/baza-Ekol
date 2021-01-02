b1c536758ffc3d8722b22d7ad3e16fee0e8ff64c
TPogacar <pogacar.tami@gmail.com>
2021-01-03 00:09:07 +0100

    spletni vmesnik

---
+++ spletni_vmesnik.py
@@ -0,0 +1,179 @@
+from model import *
+import sqlite3
+import os
+from bottle import *
+
+
+app = default_app()
+
+
+# STATIC -----------------------------------------------------------------------------------------------
+@get('/static/<filename:path>')
+def server_static(filename):
+    return static_file(filename, root='static')
+
+
+# -------------------------------------------------------------------------------------------------------
+# SPLETNE STRANI ----------------------------------------------------------------------------------------
+
+# ZAČETNA STRAN -----------------------------------------------------------------------------------------
+@route('')
+@route('/')
+@route('/domov')
+@get('/zacetna_stran')
+def zacetna_stran():
+    return template('zacetna_stran.html')
+
+
+@route('/izbira_dejavnosti')
+def izbira_dejavnosti():
+    return '''
+        <form action="/izbira_dejavnosti" method="post">
+            <select value="dejavnost" type="text" />
+            <input value="dejavnost" type="submit" />
+        </form>
+    '''
+
+@route('/izbira_dejavnosti', method='POST')
+@route('/uvoz_odpadka')
+@route('/odvoz_odpadka')
+@route('/pregled')
+def izberi():
+    dejavnost = request.forms.get('dejavnost')
+    if dejavnost == 'uvoz':
+        # return "<p>Preusmerjam ...</p>"
+        return template('uvoz_odpadka.html')
+    elif dejavnost == 'izvoz':
+        return template('izvoz_odpadka.html')
+    elif dejavnost == 'pregled':
+        return template('pregled.html')
+    else:  # dejavnost == ''
+        return "<p>Neveljavna izbira!</p>"
+
+
+# UVOZ ODPADKA ------------------------------------------------------------------------------------------
+@get('/uvoz_odpadka')
+def uvoz_odpadka():
+    return template('uvoz_odpadka.html')
+
+
+@route('/podatki_o_odpadku')
+def podatki_o_odpadku():
+    return '''
+        <form action="/podatki_o_odpadku" method="post">
+
+            <select value="teza" type="int" />
+            <input value="teza" type="submit" />
+
+            <select value="povzrocitelj" type="text" />
+            <input value="povzrocitelj" type="submit" />
+
+            <select value="datum_uvoza" type="date" />
+            <input value="datum_uvoza" type="submit" />
+
+            <select value="klasifikacijska_stevilka" type="text" />
+            <input value="klasifikacijska_stevilka" type="submit" />
+
+            <select value="skladisce" type="int" />
+            <input value="skladisce" type="submit" />
+
+            <select value="opomba_uvoza" type="text" />
+            <input value="opomba_uvoza" type="submit" />
+
+        </form>
+    '''
+
+
+@route('/podatki_o_odpadku', method='POST')
+def dodaj_odpadek():
+    teza = request.forms.get('teza')
+    povzrocitelj = request.forms.get('povzrocitelj')
+    datum_uvoza = request.forms.get('datum_uvoza')
+    klasifikacijska_stevilka = request.forms.get('klasifikacijska_stevilka')
+    skladisce = request.forms.get('skladisce')
+    opomba_uvoza = request.forms.get('opomba_uvoza')
+    
+    odpadek = Odpadek(teza, klasifikacijska_stevilka, skladisce,
+     datum_uvoza, povzrocitelj, opomba_uvoza)
+    
+    odpadek.dodaj_v_bazo()
+
+    return template('zacetna_stran.html')
+
+
+# ODVOZ ODPADKA -----------------------------------------------------------------------------------------
+@get('/izvoz_odpadka')
+def odvoz_odpadka():
+    return template('izvoz_odpadka.html')
+
+
+@route('/podatki_o_odpadku_izvoz')
+def podatki_o_odpadku_izvoz():
+    return '''
+        <form action="/podatki_o_odpadku" method="post">
+
+            <select value="teza" type="int" />
+            <input value="teza" type="submit" />
+
+            <select value="klasifikacijska_stevilka" type="text" />
+            <input value="klasifikacijska_stevilka" type="submit" />
+
+            <select value="datum_uvoza" type="date" />
+            <input value="datum_uvoza" type="submit" />
+
+            <select value="skladisce" type="int" />
+            <input value="skladisce" type="submit" />
+
+            <select value="prejemnik" type="text" />
+            <input value="prejemnik" type="submit" />
+
+            <select value="datum_izvoza" type="date" />
+            <input value="datum_izvoza" type="submit" />
+
+            <select value="opomba_izvoza" type="text" />
+            <input value="opomba_izvoza" type="submit" />
+
+        </form>
+    '''
+
+
+@route('/podatki_o_odpadku_izvoz', method='POST')
+def izvozi_odpadek():
+    teza = request.forms.get('teza')
+    klasifikacijska_stevilka = request.forms.get('klasifikacijska_stevilka')
+    datum_uvoza = request.forms.get('datum_uvoza')
+    skladisce = request.forms.get('skladisce')
+    datum_izvoza = request.forms.get('datum_izvoza')
+    prejemnik = request.forms.get('prejemnik')
+    opomba_izvoza = request.forms.get('opomba_izvoza')
+    
+    odpadek = Odpadek(teza, klasifikacijska_stevilka, skladisce,
+     datum_uvoza)
+    
+    #odpadek.izvozi(datum_izvoza, opomba_izvoza, prejemnik)
+    print(odpadek)
+    return template('zacetna_stran.html')
+
+
+
+
+
+
+
+# PREGLED SKLADIŠČENIH ODPADKOV --------------------------------------------------------------------------
+
+
+
+
+
+# -----------------------------------------------------------------------------------------------------
+# @error(404)
+# def error404(error):
+#     return 'Nothing here, sorry.'
+
+
+# ----------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    run(app, host='localhost', port=port, debug=True, reloader=True)
+
---
+++ views/osnova.tpl
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Skladišče podjetja Ekol</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="Osnovna stran skladišča Ekol" />
+		<meta name="author" content="datamate">
+		<meta name="keywords" content="Ekol" />
+		
+		<link rel="stylesheet" href='../static/assets/css/main.css'/>
+
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<a class="logo" href="domov">Ekol</a>
+				<nav>
+					<a href="#menu">Meni</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="domov">Domov</a></li>
+					<li><a href="uvoz_odpadka">Uvoz odpadka</a></li>
+					<li><a href="izvoz_odpadka">Izvoz odpadka</a></li>
+				</ul>
+			</nav>
+
+		<!-- Heading -->
+			<div id="heading" >
+				<h1>SKLADIŠČE</h1>
+			</div>
+
+		<!-- Main -->
+			
+
+        {{!base}}
+
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<div class="content">
+						<section>
+							<h4>Koristna povezava</h4>
+							<ul class="plain">
+								<li> <a href="https://ekorel.ekol.si/" align="right"  >Ekol d.o.o.</a> </li>
+							</ul>
+						</section>
+					</div>
+					<div class="copyright">
+						% data = {"developer_name": "Tamara Pogačar in Matej Čušin", "developer_organization": "Praktična matematika, FMF"}
+						% end
+						&copy; 2021, {{data["developer_name"]}} ({{data["developer_organization"]}})
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="../static/assets/js/jquery.min.js"></script>
+			<script src="../static/assets/js/browser.min.js"></script>
+			<script src="../static/assets/js/breakpoints.min.js"></script>
+			<script src="../static/assets/js/util.js"></script>
+			<script src="../static/assets/js/main.js"></script>
+		
+	</body>
+</html>
